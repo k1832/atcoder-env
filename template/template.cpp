4c6cff41f17dcd382cff58db9bd9fe25dfc65009
Keita <keyz@google.com>
2023-04-03 05:57:40 +0000

    Update hash function for pair

--- template/template.cpp
+++ template/template.cpp
@@ -6,15 +6,21 @@ using ll = long long;
 using P = pair<int, int>;
 template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
 template<class T> using max_heap = priority_queue<T, std::vector<T>>;
+
+// Use this struct to use pair<T1, T2> as key of hash [map|set]
+// Example: unordered_map<std::pair<int, int>, int, pair_hash> ump;
 struct pair_hash {
-    // e.g. unordered_map<std::pair<int, int>, int, pair_hash> ump;
     template <class T1, class T2>
     std::size_t operator() (const std::pair<T1, T2>& p) const {
         auto h1 = std::hash<T1>{}(p.first);
         auto h2 = std::hash<T2>{}(p.second);
 
         // Combine the two hashes (example taken from boost::hash_combine)
-        return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
+        // Another ref: https://github.com/HowardHinnant/hash_append/issues/7
+        if (sizeof(T1) >= 8)
+            return h1 ^ (h2 + 0x9e3779b97f4a7c15ULL + (h1 << 12) + (h1 >> 4));
+        else
+            return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
     }
 };
 
