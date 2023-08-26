8e2df3852fd344ece582dc4ad823fe84333c2967
Keita <keyz@google.com>
2023-08-26 13:48:56 +0000

    Remove pair_hash

--- template/template.cpp
+++ template/template.cpp
@@ -7,24 +7,6 @@ using P = pair<int, int>;
 template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
 template<class T> using max_heap = priority_queue<T, std::vector<T>>;
 
-// Use this struct to use pair<T1, T2> as key of hash [map|set]
-// Example: unordered_map<std::pair<int, int>, int, pair_hash> ump;
-// Example: unordered_map<std::pair<int, int>, pair_hash> ump;
-struct pair_hash {
-    template <class T1, class T2>
-    std::size_t operator() (const std::pair<T1, T2>& p) const {
-        auto h1 = std::hash<T1>{}(p.first);
-        auto h2 = std::hash<T2>{}(p.second);
-
-        // Combine the two hashes (example taken from boost::hash_combine)
-        // Another ref: https://github.com/HowardHinnant/hash_append/issues/7
-        if (sizeof(T1) > 8)
-            return h1 ^ (h2 + 0x9e3779b97f4a7c15ULL + (h1 << 12) + (h1 >> 4));
-        else
-            return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
-    }
-};
-
 #define LOG(variable) cerr << #variable":\t" << (variable) << "\n"
 #define LOGCON(i, container) for(size_t (i) = 0; (i) < (container).size(); ++(i)) cerr << (i) << ":\t" << (container)[(i)] << "\n"
 #define REP(i, n) for (long long i = 0; i < (n); ++i)
